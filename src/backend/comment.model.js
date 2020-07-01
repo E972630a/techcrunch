f8bf55cafed281e9fd10274cbebb6166a84ef9eb
Esau Aguilar Osuna <esauaguilar160@gmail.com>
2020-06-30 22:10:27 -0400

    comment changes

--- src/backend/comment.model.js
+++ src/backend/comment.model.js
@@ -2,23 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 //Add mongoose schema to access MongoDB in an object oriented way
 let Comment = new Schema({
-<<<<<<< HEAD
-<<<<<<< HEAD
-    comment_name: {
-        type: String
-    },
-    comment_description: {
-=======
-=======
->>>>>>> da1f36f0c9dd5aaed6815e2013c9bbd679f01902
+
     comment_name1: {
         type: String
     },
     comment_description1: {
-<<<<<<< HEAD
->>>>>>> ea8998c00aa16cc83ffc2ad6818ec1531285f336
-=======
->>>>>>> da1f36f0c9dd5aaed6815e2013c9bbd679f01902
+
         type: String
     },
     comment_name2: {
@@ -26,11 +15,7 @@ let Comment = new Schema({
     },
     comment_description2: {
         type: String
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> da1f36f0c9dd5aaed6815e2013c9bbd679f01902
+
     },
     comment_name3: {
         type: String
@@ -49,10 +34,7 @@ let Comment = new Schema({
     },
     comment_description5: {
         type: String
-<<<<<<< HEAD
->>>>>>> ea8998c00aa16cc83ffc2ad6818ec1531285f336
-=======
->>>>>>> da1f36f0c9dd5aaed6815e2013c9bbd679f01902
+
     }
 
 
